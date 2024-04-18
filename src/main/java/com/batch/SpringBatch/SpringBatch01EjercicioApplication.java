29ea47b3091ef0975f33ae0a3532dfcb9efc5a0f
Antonio-LAndrade-Babel <antonio.landrade@babelgroup.com>
2024-04-18 11:13:57 +0200

    Commit inicial

---
+++ src/main/java/com/batch/SpringBatch/SpringBatch01EjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.batch.SpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch01EjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch01EjercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/SpringBatch/SpringBatch01EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.SpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch01EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
